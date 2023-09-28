cfc016f8ec29f405223c17e7aaafc7b1cae7c922
biellofy <gabrielmoraes12334@gmail.com>
2023-09-27 23:54:22 -0300

    #6 - Added userService to connect to the database

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,21 +1,30 @@
-const create = (req, res) => {
-  const { name, username, email, password, avatar, background } = req.body
+import userService from "../services/user.service.js";
+
+const create = async (req, res) => {
+  const { name, username, email, password, avatar, background } = req.body;
 
   if (!name || !username || !email || !password || !avatar || !background) {
-    res.status(400).send({ message: 'Submit all fields for registration' })
+    res.status(400).send({ message: "Submit all fields for registration" });
+  }
+
+  const user = await userService.create(req.body);
+
+  if (!user) {
+    return res.status(400).send({ message: "Error creating User" });
   }
 
   res.status(201).send({
-    message: 'User created',
+    message: "User created",
     user: {
+      id: user._id,
       name,
       username,
       email,
       password,
       avatar,
-      background
-    }
-  })
-}
+      background,
+    },
+  });
+};
 
-export default { create }
+export default { create };
