5a1b113a2acc456900ee5d1258fcd9abadd57ef5
biellofy <gabrielmoraes12334@gmail.com>
2023-10-07 10:22:12 -0300

    14 - Creating a jwt token in an express api

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import { loginService } from "../services/auth.service.js";
+import { loginService, generateToken } from "../services/auth.service.js";
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -17,9 +17,11 @@ const login = async (req, res) => {
       return res.status(400).send({ message: "User or password not found" });
     }
 
-    res.send({ message: "Login OK" });
+    const token = generateToken(user.id)
+
+    res.send({token});
   } catch (err) {
-    res.status(500).send(err.message);
+    res.status(500).send({ message: message.err });
   }
 };
 
